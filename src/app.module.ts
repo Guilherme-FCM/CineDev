fc7a608d5d42db353eb8cf7e1e0ddfa5f9e142c3
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-22 16:54:30 -0300

    Add docker postgres database service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'docker',
+      password: 'root',
+      database: 'cine-dev',
+      entities: [],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
