691e1983585043f8c0e526f0bafe7101fdb1a4ed
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-22 15:57:48 -0300

    add docker and docker compose

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:14-alpine
+
+WORKDIR /api
+
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD [ "npm", "run", "start:dev" ]
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello World.';
+    return 'Hello World by Docker!';
   }
 }
